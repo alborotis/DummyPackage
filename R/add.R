42873dbdc8adf51f6678f7ef46c35ee71de71594
alborotis <alborotis@gmail.com>
2016-11-09 12:08:39 +0100

    Add files via upload

---
+++ R/add.R
@@ -0,0 +1,12 @@
+#' add
+#'
+#' Takes in a numeric list and adds up all the values in the list
+#'
+#' @name add
+#' @param x: numeric list
+#' @export
+
+
+add <- function(x){
+  sum(x)
+}
---
+++ tests/testthat/test-add.R
@@ -0,0 +1,1 @@
+expect_equal(add(c(1,2,3)), 6)
